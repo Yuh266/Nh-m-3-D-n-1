2bb4fc06efe5be592512f4246103c96f53530ecc
Nguyen Quang Huy <nhuy04625@gmail.com>
2024-11-02 14:16:57 +0700

    model_loai_hang_CRUD

--- admin/controllers/AdminLoaiHangController.php
+++ admin/controllers/AdminLoaiHangController.php
@@ -22,9 +22,6 @@ class AdminLoaiHangController{
                 echo "Lỗi";
             }
          }
-
-
-
     }
 
 }
--- admin/models/AdminLoaiHang.php
+++ admin/models/AdminLoaiHang.php
@@ -7,6 +7,22 @@ class AdminLoaiHang{
         $this->conn = connectDB();
     }
 
+    public function getAllLoaiHang(){
+        $sql = "SELECT * FROM loai_hang";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+
+    public function getLoaiHangByID($id){
+        $sql = "SELECT * FROM loai_hang WHERE id=".$id;
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
     public function insertLoaiHang($ten_loai_hang, $mo_ta){
         try {
             $sql = "INSERT INTO loai_hang(ten_loai_hang, mo_ta) VALUES(:ten_loai_hang, :mo_ta)";
@@ -18,6 +34,30 @@ class AdminLoaiHang{
             echo"Lôi". $th->getMessage();
         }
 
+    }
+    
+    public function updateLoaiHang($id,$ten_loai_hang, $mo_ta){
+        try {
+            $sql = "UPDATE loai_hang SET ten_loai_hang = :ten_loai_hang , mo_ta = :mo_ta WHERE id =".$id;
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([":ten_loai_hang"=>$ten_loai_hang,"mo_ta"=>$mo_ta]);
+            
+            return true;
+        }catch (Exception $th) {
+            echo "Lỗi". $th->getMessage();
+        }
+    }
+
+    public function deleteLoaiHang($id){
+        try {
+            $sql = "DELETE FROM loai_hang WHERE id=".$id;
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+
+            return true;
+        }catch (Exception $th) {
+            echo "Lỗi". $th->getMessage();
+        }
     }
 
 
