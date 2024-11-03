a428acc9d18330c72b5aec937f7f9a7554f946cf
Nguyen Quang Huy <nhuy04625@gmail.com>
2024-11-03 22:22:44 +0700

    update layout

--- admin/views/LoaiHang/addLoaiHang.php
+++ admin/views/LoaiHang/addLoaiHang.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thêm</title>
-</head>
-<body>
+<?php include './views/layout/header.php' ?>
+
+<?php include './views/layout/navbar.php' ?>
+
+<main>
     <form action="<?= BASE_URL_ADMIN.'?act=them-loai-hang' ?>" method="post">
         <input placeholder="ten_loai_hang" name="ten_loai_hang" type="text">
         <input placeholder="mo_ta" name="mo_ta" type="text">
         <button type="submit" >Thêm</button>
     </form>
-</body>
-</html>
+</main>
+
+<?php include './views/layout/footer.php' ?>
