f838cdce08d51b93ebb6d89ae015b3dba3eda51b
Tun52299 <tuanpaph52299@gmail.com>
2024-11-09 09:28:20 +0700

    fix_navbar

--- admin/views/layout/navbar.php
+++ admin/views/layout/navbar.php
@@ -11,31 +11,25 @@
             </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
             <div class="sidebar-wrapper">
                 <nav class="mt-2"> <!--begin::Sidebar Menu-->
-                    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item menu-open"> <a href="#" class="nav-link active"> <i class="nav-icon bi bi-speedometer"></i>
-                                <p>
-                                    Dashboard
-                                    <i class="nav-arrow bi bi-chevron-right"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item"> <a href="<?= BASE_URL_ADMIN.'?act=danh-sach-danh-muc' ?>" class="nav-link active"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>Danh mục sản phẩm</p>
-                                    </a> </li>
-                                <li class="nav-item"> <a href="<?= BASE_URL_ADMIN ?>" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>Sản phẩm</p>
-                                    </a> </li>
-                                <li class="nav-item"> <a href="./index3.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>Dashboard v3</p>
-                                    </a> </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"> <a href="<?= BASE_URL_ADMIN.'?act=danh-sach-danh-muc' ?>" class="nav-link"> <i class="nav-icon bi bi-palette"></i>
-                                <p>Theme Generate</p>
-                            </a> </li>
-                        <li class="nav-item"> <a href="<?= BASE_URL_ADMIN . "/?act=danh-sach-slide-show" ?>" class="nav-link"> <i class="nav-icon bi bi-palette"></i>
-                                <p>Quản trị slide shows</p>
-                            </a> </li>
+                    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">                      
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL_ADMIN . '?act=danh-sach-danh-muc' ?>" class="nav-link <?= ($_GET['act'] == 'danh-sach-danh-muc') ? 'active' : '' ?>">
+                            <i class="nav-icon bi bi-circle"></i>
+                            <p>Quản trị danh mục</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL_ADMIN . '?act=danh-sach-san-pham' ?>" class="nav-link <?= ($_GET['act'] == 'danh-sach-san-pham') ? 'active' : '' ?>">
+                            <i class="nav-icon bi bi-circle"></i>
+                            <p>Quản trị sản phẩm</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL_ADMIN . '?act=danh-sach-slide-show' ?>" class="nav-link <?= ($_GET['act'] == 'danh-sach-slide-show') ? 'active' : '' ?>">
+                            <i class="nav-icon bi bi-palette"></i>
+                            <p>Quản trị slide shows</p>
+                        </a>
+                    </li>
                         <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-box-seam-fill"></i>
                                 <p>
                                     Widgets
