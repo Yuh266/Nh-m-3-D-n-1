0a7c574a6e18671fa0c0d7d256c9e6bf0b6b1032
Nguyen Quang Huy <nhuy04625@gmail.com>
2024-11-12 21:28:00 +0700

    Đổi tên thành phần  heder

---
+++ controllers/SanPhamController.php
@@ -0,0 +1,14 @@
+<?php 
+
+class SanPhamController
+{
+    public function __construct() {
+        
+    }
+
+    public function Trangchu(){
+
+        require './views/TrangChinh/trangchu.php' ;
+    }
+
+}
